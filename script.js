bc6b04bdc48fdc01a2e4ae72cc1e1b7c69598d0e
KagisoMolefeSa <janzito19molefe@gmail.com>
2026-07-01 12:33:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function searchGrave() {
 
+    const search = document.getElementById("searchInput").value;
+    const results = document.getElementById("results");
+
+    if (search === "") {
+        results.innerHTML =
+            "<p>Please enter a name or grave number.</p>";
+        return;
+    }
+
+    results.innerHTML =
+        "<h3>Searching for: " + search + "</h3>" +
+        "<p>This will soon search the GraveFinder SA database.</p>";
+
+}
